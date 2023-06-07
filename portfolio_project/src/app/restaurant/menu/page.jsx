60d83471b08228f9fcb1abdb2020ff9a27c01b13
SemTheLegend <semmathe@gmail.com>
2023-06-07 19:51:44 +0200

    created webpage navigation files

---
+++ portfolio_project/src/app/restaurant/menu/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () = {
+  return (
+    <div>
+      Food Menu
+    </div>
+  );
+};
+
+export default Menu;
---
+++ portfolio_project/src/app/user/orders/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+  return (
+    <div>
+      Order History Page
+    </div>
+  );
+};
+
+export default Orders;
