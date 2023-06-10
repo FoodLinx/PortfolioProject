ca5164b6db61dc265d03b6a1659a635e3a696b76
SemTheLegend <semmathe@gmail.com>
2023-06-10 09:50:33 +0200

    removed components

--- portfolio_project/src/app/layout.js
+++ portfolio_project/src/app/layout.js
@@ -1,7 +1,5 @@
-import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,9 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className='container'>
-          <Navbar />
           {children}
-          <Footer />
         </div>
       </body>
     </html>
