47cf2c1d95cd12c091b7c8be934113f241549e95
Elijah Sinkamba <scengj@gmail.com>
2023-06-11 14:01:03 +0200

    Navbar component

---
+++ portfolio_project/src/components/Navbar/Navbar.js
@@ -0,0 +1,43 @@
+"use client"
+
+import React from 'react'
+import nav from './navbar.module.scss'
+import Link from 'next/link'
+import { ImCart } from 'react-icons/im'
+
+export const Navbar = () => {
+  return (
+    <div className={nav.navbar}>
+      <div className={nav.logo}>
+        <Link href='/'>
+          FoodLinx
+        </Link>
+      </div>
+      <div className={nav.link}>
+        <div className={nav.links}>
+          <Link 
+          href='/'
+          >
+            Home
+          </Link>
+          <Link 
+          href='/'
+          >
+            About Us
+          </Link>
+          <Link 
+          href='/'
+          >
+            Contact
+          </Link>
+        </div>
+        <div className={nav.login_cart}>
+          <button className={nav.login}>Login</button>
+          <div className={nav.cart}>
+            <ImCart />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
