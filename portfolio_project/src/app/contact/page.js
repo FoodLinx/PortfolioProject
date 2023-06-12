226b578364902c492835a06e3deb5c2870a0573e
Elijah Sinkamba <scengj@gmail.com>
2023-06-12 11:19:24 +0200

    contact page

--- portfolio_project/src/app/contact/page.js
+++ portfolio_project/src/app/contact/page.js
@@ -7,7 +7,7 @@ const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add code to send the email
@@ -21,45 +21,54 @@ const Contact = () => {
 
   return (
     <div className={contact.container}>
-      <h2 className={contact.h2}>Contact Us</h2>
-      <form className={contact.form} onSubmit={handleSubmit}>
-        <div className={contact.formGroup}>
-          <label 
-          className={contact.label}
-          htmlFor="name">Name:</label>
-          <input
-            className={contact.input}
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className={contact.formGroup}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className={contact.formGroup}>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            className={contact.textarea}
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          ></textarea>
-        </div>
-        <button type="submit" className={contact.submitButton}>
-          Send Message
-        </button>
-      </form>
+      <div className={contact.greeting}>
+        <h2 className={contact.heading} >Get in Touch</h2>
+        <p className={contact.text} >
+          We would love to hear from you! Whether you have a question, feedback,
+          or simply want to say hello, fill out the form below and we'll get back
+          to you as soon as possible.
+        </p>
+      </div>
+      <div className={contact.formContainer}>
+        <h2 className={contact.heading}>Contact Us</h2>
+        <form className={contact.form} onSubmit={handleSubmit}>
+          <div className={contact.formGroup}>
+            <label className={contact.label} htmlFor="name">Name:</label>
+            <input
+              className={contact.input}
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className={contact.formGroup}>
+            <label className={contact.label} htmlFor="email">Email:</label>
+            <input
+              className={contact.input}
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className={contact.formGroup}>
+            <label className={contact.label} htmlFor="message">Message:</label>
+            <textarea
+              className={contact.textarea}
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            ></textarea>
+          </div>
+          <button type="submit" className={contact.submitButton}>
+            Send Message
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
