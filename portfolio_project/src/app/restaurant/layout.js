ca6f007995c0e84dcad5dc9dbde9d51b465d5d20
SemTheLegend <semmathe@gmail.com>
2023-06-13 14:04:34 +0200

    restaurant home page

--- portfolio_project/src/app/restaurant/layout.js
+++ portfolio_project/src/app/restaurant/layout.js
@@ -1,25 +1,25 @@
-import { RestaurantFooter } from '../components/Footer/RestaurantFooter'
-import { RestaurantNavbar } from '../components/Navbar/RestaurantNavbar'
-import './page.module.css'
-import { Inter } from 'next/font/google'
+import RestaurantNavbar from "@/src/components/Navbar/RestaurantNavbar";
+import RestaurantFooter from "@/src/components/Footer/RestaurantFooter";
+import "./page.module.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'FoodLinx',
-  description: 'Online Food Delivery Software/Application',
-}
+  title: "FoodLinx",
+  description: "Online Food Delivery Software/Application",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='container'>
-          <RestaurantNavbar/>
+        <div className="container">
+          <RestaurantNavbar />
           {children}
-          <RestaurantFooter/>
+          <RestaurantFooter />
         </div>
       </body>
     </html>
-  )
+  );
 }
