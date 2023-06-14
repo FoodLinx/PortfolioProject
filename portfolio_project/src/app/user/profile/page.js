a27e2eaa7b3474eb8fc06fc0fce5d55d53a9473c
Elijah Sinkamba <scengj@gmail.com>
2023-06-14 08:37:58 +0200

    added ffetch of data and displaying it removed data change feature

--- portfolio_project/src/app/user/profile/page.js
+++ portfolio_project/src/app/user/profile/page.js
@@ -1,36 +1,23 @@
 "use client"
 
-import React, { useState } from 'react';
+
+import React, { useEffect, useState } from 'react';
 import styles from './profile.module.scss';
-import burger from '@/public/Burgers/burger.jpg';
+import axios from 'axios';
 
 const Profile = () => {
-  const [name, setName] = useState('John Doe');
-  const [phoneNumber, setPhoneNumber] = useState('1234567890');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('john.doe@example.com');
+  const [userData, setUserData] = useState(null);
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-
-  const handlePhoneNumberChange = (event) => {
-    setPhoneNumber(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // TODO: Save the updated details to the database
-    console.log('Updated details:', { name, phoneNumber, password, email });
-  };
+  useEffect(() => {
+    // Fetch user data from the API
+    axios.get('https://api/user')
+      .then(response => {
+        setUserData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -38,52 +25,46 @@ const Profile = () => {
         <h1 className={styles.title}>My Account</h1>
       </div>
       <div className={styles.content}>
-        <div className={styles.profile}>
-          <img
-            src="https://img.freepik.com/free-photo/double-hamburger-isolated-white-background-fresh-burger-fast-food-with-beef-cream-cheese_90220-1192.jpg?w=740&t=st=1686671908~exp=1686672508~hmac=51468aa0347e01065d4895729c027d3eb00d42a055aac42a4650f221aa74be38"
-            alt="Profile"
-            className={styles.profilePicture}
-          />
-          <form onSubmit={handleSubmit} className={styles.form}>
-            <div className={styles.formGroup}>
-              <label className={styles.label}>Name:</label>
-              <input
-                type="text"
-                value={name}
-                onChange={handleNameChange}
-                className={styles.input}
-              />
-            </div>
-            <div className={styles.formGroup}>
-              <label className={styles.label}>Phone Number:</label>
-              <input
-                type="tel"
-                value={phoneNumber}
-                onChange={handlePhoneNumberChange}
-                className={styles.input}
-              />
-            </div>
-            <div className={styles.formGroup}>
-              <label className={styles.label}>Password:</label>
-              <input
-                type="password"
-                value={password}
-                onChange={handlePasswordChange}
-                className={styles.input}
-              />
-            </div>
-            <div className={styles.formGroup}>
-              <label className={styles.label}>Email:</label>
-              <input
-                type="email"
-                value={email}
-                onChange={handleEmailChange}
-                className={styles.input}
-              />
-            </div>
-            <button type="submit" className={styles.submitButton}>Save</button>
-          </form>
-        </div>
+        {userData ? (
+          <div className={styles.profile}>
+            <img
+              src={userData.profilePicture}
+              alt="Profile"
+              className={styles.profilePicture}
+            />
+            <form className={styles.form}>
+              <div className={styles.formGroup}>
+                <label className={styles.label}>Name:</label>
+                <input
+                  type="text"
+                  value={userData.name}
+                  className={styles.input}
+                  disabled
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label className={styles.label}>Phone Number:</label>
+                <input
+                  type="tel"
+                  value={userData.phoneNumber}
+                  className={styles.input}
+                  disabled
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label className={styles.label}>Email:</label>
+                <input
+                  type="email"
+                  value={userData.email}
+                  className={styles.input}
+                  disabled
+                />
+              </div>
+            </form>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </div>
   );
